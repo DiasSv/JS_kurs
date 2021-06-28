4772d7949701d1c6d902d87e7b121740a37880cb
DiasSv <dima.sveshnikov.81@mail.ru>
2021-06-28 21:10:54 +0300

    Добавление необходимых для работы файлов

---
+++ js/script.js
@@ -0,0 +1,48 @@
+"use strict";
+
+/*
+let number = 5;
+const left = 1;
+
+number = 10;
+console.log(number);
+*/
+
+
+// Всплывающее окно и вывод ответа пользователя на алерт
+/*
+const result = confirm("Are u here?");
+console.log(result);
+*/
+
+/*
+const answer = prompt("are u 18 age?", "ea"); // этот алерт дает возможность пользователю написать свой ответ
+console.log(answer);  // а если добавить + перед prompt, то произойдет конвертация строки к числу
+*/
+
+/*  
+const answers = [];   
+
+answers[0] = prompt("how your name?", "");
+answers[1] = prompt("how your lastname?", "");
+answers[2] = prompt("how your age?", "");
+
+document.write(answers);
+console.log(typeof(answers));
+*/
+
+/*
+const category = 'toys'; //интерполяция
+
+console.log(`https://someurl.com/${category}/5`);
+*/
+
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
